81c661a67ebcca3154e191a92016f7a0b2c88e9d
You Name <yourmail@server.com>
2016-03-04 16:44:50 +0800

    Initial commit

---
+++ src/com/test/wl/MainAcvtivity.java
@@ -0,0 +1,15 @@
+package com.test.wl;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+public class MainAcvtivity extends Activity{
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+	}
+}
